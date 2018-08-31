0388efe7ea8f23dad4f40f6b926cd76d2b0e008a
Ewan Pinnington <ewan.pinnington@gmail.com>
2018-08-31 11:51:23 +0100

    updated version and upload to pypi for pip install

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.3',  # Required
+    version='0.2.4',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -117,7 +117,7 @@ setup(
                       'pyorbital',
                       'mock',
                       'matplotlib',
-                      'netCDF4'
+                      'netCDF4',
                       'f90nml'],  # Optional
 
     # If there are data files included in your packages that need to be
