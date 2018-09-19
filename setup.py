a23ffbe33c2edfc6f3cdcbbecab956311c126a91
Ewan Pinnington <ewan.pinnington@gmail.com>
2018-09-19 16:46:18 +0100

    updated site.nml and setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.5',  # Required
+    version='0.2.6',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -128,7 +128,7 @@ setup(
     package_data={  # Optional
         'signaturesimulator': ['site.nml', 'data/tle/*', 'data/srf/*', 'data/state_variables/*', 'data/geometries/*',
                                'sense/*.py', 'sense/dielectric/*', 'sense/surface/*', 'sense/vegetation/*',
-                               'semidiscrete_srf/*', 'semidiscrete_srf/man/*',
+                               'semidiscrete_srf/semiD', 'semidiscrete_srf/man/*',
                                'data/jules/example_nml/*', 'data/jules/jules_data/*', 'data/jules/output/*']
     },
     cmdclass={'install': MyInstall},
